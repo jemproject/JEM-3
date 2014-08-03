63cc95eb42f481f573d5fbd61c34788d2c0721f3
Bluefox <jaapkoelman@hotmail.com>
2014-08-03 10:33:27 +0200

    added field-mathquiz+rule-mathquiz/timeout

---
+++ admin/models/fields/mathquiz.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @version 3.0.1
+ * @package JEM
+ * @copyright (C) 2013-2014 joomlaeventmanager.net
+ * @copyright (C) 2005-2009 Christoph Lukes
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Mathquiz
+ */
+class JFormFieldMathquiz extends JFormField
+{
+	private $operand1, $operand2;
+	
+	/**
+	 * The form field type.
+	 */
+	protected $type = 'MathQuiz';
+
+	/**
+	 * Method to get the field input markup.
+	 */
+	protected function getInput()
+	{		
+		$this->generateNumbers();
+		
+		$label = '<span id="mathquiz">'.$this->operand1 . ' + ' . $this->operand2.'</span>';
+		//$math = '<input type="text" name="' . $this->name . '" value="" id="' . $this->name . '"></input>';
+			
+		$answer = '<input type="hidden" name="' . $this->name . '-answer" value="' . ( $this->operand1 + $this->operand2 ) . '"></input>';
+		$string = $label . "\n" . $answer;
+			
+			
+		return $string;
+	}
+	
+	
+	public function generateNumbers()
+	{
+		$this->operand1 = mt_rand( 1, 9 );
+		$this->operand2 = mt_rand( 1, 9 );
+	}
+}
---
+++ admin/models/rule/mathquiz.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @version 3.0.1
+ * @package JEM
+ * @copyright (C) 2013-2014 joomlaeventmanager.net
+ * @copyright (C) 2005-2009 Christoph Lukes
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * MathQuiz
+ */
+class JFormRuleMathquiz extends JFormRule
+{
+	/**
+	 * Method to test if the answer is correct.
+	 * 
+	 * 
+	 * @param   SimpleXMLElement  $element  The SimpleXMLElement object representing the <field /> tag for the form field object.
+	 * @param   mixed             $value    The form field value to validate.
+	 * @param   string            $group    The field name group control value. This acts as as an array container for the field.
+	 *                                      For example if the field has name="foo" and the group value is set to "bar" then the
+	 *                                      full field name would end up being "bar[foo]".
+	 * @param   JRegistry         $input    An optional JRegistry object with the entire data set to validate against the entire form.
+	 * @param   JForm             $form     The form object for which the field is being tested.
+	 *
+	 * @return  boolean  True if the value is valid, false otherwise.
+	 * 
+	 */
+	public function test(SimpleXMLElement $element, $value, $group = null, JRegistry $input = null, JForm $form = null)
+	{
+		$jinput 	= JFactory::getApplication()->input;
+		$data  		= $jinput->post->get('jform', array(), 'array');
+		
+		$question	= $data['mathquiz'];
+		$answer		= $data['mathquiz_answer'];
+		
+		// Test the value.
+		if (!($question == $answer))
+		{
+			return false;
+		}
+
+		return true;
+	}
+}
