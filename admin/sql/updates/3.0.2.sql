de14ba4284da6d641c58321ef3a3aa8aefc7be13
Bluefox <jaapkoelman@hotmail.com>
2014-08-08 13:49:20 +0200

    added ADD statement to update sql

--- admin/sql/updates/3.0.2.sql
+++ admin/sql/updates/3.0.2.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `#__jem_events`
-	`recurrence_count` int(11) NOT NULL,
-	`recurrence_weekday` varchar(100) NOT NULL;
+	ADD `recurrence_count` int(11) NOT NULL,
+	ADD `recurrence_weekday` varchar(100) NOT NULL;
