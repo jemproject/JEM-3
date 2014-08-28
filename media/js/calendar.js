9bbba907c9215939a0b395a5bb79798e4248ecd0
Bluefox <jaapkoelman@hotmail.com>
2014-08-28 10:34:15 +0200

    added header to calendar.js

--- media/js/calendar.js
+++ media/js/calendar.js
@@ -1,3 +1,11 @@
+/**
+ * @version 3.0.2
+ * @package JEM
+ * @copyright (C) 2013-2014 joomlaeventmanager.net
+ * @copyright (C) 2005-2009 Christoph Lukes
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
 jQuery(document).ready(function() {
 	
 	/* define background */
