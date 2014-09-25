cf0a227eb817f29ca5ef746d7123fac998f006ff
Bluefox <jaapkoelman@hotmail.com>
2014-09-25 08:32:41 +0200

    altered update sql, REF #108

--- admin/sql/updates/3.0.3.sql
+++ admin/sql/updates/3.0.3.sql
@@ -5,3 +5,20 @@
 UPDATE #__jem_settings
 SET `version`='3.0.3'
 WHERE `version`='3.0.1';
+
+
+ALTER TABLE `#__jem_settings`
+	DROP `datdesclimit`,
+	DROP `discatheader`,
+	DROP `showtime`,
+	DROP `showtimedetails`,
+	ADD `vevent` text NOT NULL DEFAULT '',
+  	ADD `vvenue` text NOT NULL DEFAULT '',
+  	ADD `vvenues` text NOT NULL DEFAULT '',
+  	ADD `vcategories` text NOT NULL DEFAULT '',
+  	ADD `vcategory` text NOT NULL DEFAULT '';
+	
+ALTER TABLE `#__jem_events`
+	ADD startDateTime varchar(100) NOT NULL DEFAULT '',
+	ADD endDateTime varchar(100) NOT NULL DEFAULT '',
+	ADD note varchar(100) NOT NULL DEFAULT '';
