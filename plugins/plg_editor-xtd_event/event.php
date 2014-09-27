a01c5b3feb8c9ddb60cf811a1cb1fa40225369bc
Bluefox <jaapkoelman@hotmail.com>
2014-09-27 22:19:55 +0200

    added editor-xtd plugin (event)

---
+++ plugins/plg_editor-xtd_event/event.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * @version 3.0.2
+ * @package JEM
+ * @subpackage JEM editors-xtd plugin (event)
+ * @copyright (C) 2013-2014 joomlaeventmanager.net
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+defined('_JEXEC') or die;
+
+/**
+ * Editor Event buton
+ */
+class PlgButtonEvent extends JPlugin
+{
+	/**
+	 * Constructor
+	 *
+	 * @param   object  &$subject  The object to observe
+	 * @param   array   $config    An array that holds the plugin configuration
+	 *
+	 */
+	public function __construct(& $subject, $config)
+	{
+		parent::__construct($subject, $config);
+		$this->loadLanguage();
+	}
+	
+	
+	/**
+	 * Load the language file on instantiation.
+	 *
+	 * @var    boolean
+	 * @since  3.1
+	 */
+	protected $autoloadLanguage = true;
+
+	/**
+	 * Display the button
+	 *
+	 * @param   string  $name  The name of the button to add
+	 *
+	 * @return array A four element array of (article_id, article_title, category_id, object)
+	 */
+	public function onDisplay($name)
+	{
+		/*
+		 * Javascript to insert the link
+		 * View element calls jSelectEvent when an event is clicked
+		 * jSelectEvent creates the link tag, sends it to the editor,
+		 * and closes the select frame.
+		 */
+		$js = "
+		function jSelectEvent(id, title, object, link, lang)
+		{
+			var hreflang = '';
+			if (lang !== '')
+			{
+				var hreflang = ' hreflang = \"' + lang + '\"';
+			}
+			var tag = '<a' + hreflang + ' href=\"' + link + '\">' + title + '</a>';
+			jInsertEditorText(tag, '" . $name . "');
+			SqueezeBox.close();
+		}";
+
+		$doc = JFactory::getDocument();
+		$doc->addScriptDeclaration($js);
+
+		JHtml::_('behavior.modal');
+
+		/*
+		 * Use the built-in element view to select the event.
+		 * Currently uses blank class.
+		 */
+		$link = 'index.php?option=com_jem&amp;view=eventslist&amp;layout=modal&amp;tmpl=component&amp;' . JSession::getFormToken() . '=1';
+
+		$button = new JObject;
+		$button->modal = true;
+		$button->class = 'btn';
+		$button->link = $link;
+		$button->text = JText::_('PLG_EVENT_BUTTON_EVENT');
+		$button->name = 'calendar';
+		$button->options = "{handler: 'iframe', size: {x: 800, y: 500}}";
+
+		return $button;
+	}
+}
---
+++ site/views/eventslist/tmpl/modal.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * @version 3.0.2
+ * @package JEM
+ * @copyright (C) 2013-2014 joomlaeventmanager.net
+ * @copyright (C) 2005-2009 Christoph Lukes
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+defined('_JEXEC') or die;
+
+$app = JFactory::getApplication();
+
+if ($app->isSite())
+{
+	JSession::checkToken('get') or die(JText::_('JINVALID_TOKEN'));
+}
+
+require_once JPATH_ROOT . '/components/com_jem/helpers/route.php';
+
+JHtml::addIncludePath(JPATH_COMPONENT.'/helpers/html');
+JHtml::_('bootstrap.tooltip');
+JHtml::_('behavior.framework', true);
+
+$function  = $app->input->getCmd('function', 'jSelectEvent');
+$listOrder = $this->escape($this->state->get('list.ordering'));
+$listDirn  = $this->escape($this->state->get('list.direction'));
+?>
+<form action="<?php echo JRoute::_('index.php?option=com_jem&view=eventslist&layout=modal&tmpl=component&function='.$function.'&'.JSession::getFormToken().'=1');?>" method="post" name="adminForm" id="adminForm" class="form-inline">
+	<fieldset class="filter clearfix">
+		<div class="btn-toolbar">
+			<div class="btn-group pull-left">
+				<?php echo $this->lists['filter'].'&nbsp;'; ?>
+				<input type="text" name="filter_search" id="filter_search" value="<?php echo $this->lists['search'];?>" class="inputbox input-medium" onchange="this.form.submit();" />
+			</div>
+			<div class="btn-group pull-left">
+				<button type="submit" class="btn hasTooltip" title="<?php echo JHtml::tooltipText('JSEARCH_FILTER_SUBMIT'); ?>" data-placement="bottom">
+					<span class="icon-search"></span><?php echo '&#160;' . JText::_('JSEARCH_FILTER_SUBMIT'); ?></button>
+				<button type="button" class="btn hasTooltip" title="<?php echo JHtml::tooltipText('JSEARCH_FILTER_CLEAR'); ?>" data-placement="bottom" onclick="document.getElementById('filter_search').value='';this.form.submit();">
+					<span class="icon-remove"></span><?php echo '&#160;' . JText::_('JSEARCH_FILTER_CLEAR'); ?></button>
+			</div>
+			<div class="btn-group pull-right">
+		<?php echo $this->pagination->getLimitBox();?>
+		</div>
+			<div class="clearfix"></div>
+		</div>
+		
+	</fieldset>
+
+	<table class="table table-striped table-condensed">
+		<thead>
+			<tr>
+				<th class="title">
+					<?php echo JHtml::_('grid.sort', 'JGLOBAL_TITLE', 'a.title', $listDirn, $listOrder); ?>
+				</th>
+				<th width="15%" class="center nowrap">
+					<?php echo JHtml::_('grid.sort', 'JGRID_HEADING_ACCESS', 'access_level', $listDirn, $listOrder); ?>
+				</th>
+				<th width="15%" class="center nowrap">
+					<?php echo JText::_('JCATEGORY'); ?>
+				</th>
+				<th width="5%" class="center nowrap">
+					<?php echo JHtml::_('grid.sort', 'JDATE', 'a.dates', $listDirn, $listOrder); ?>
+				</th>
+				<th width="1%" class="center nowrap">
+					<?php echo JHtml::_('grid.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
+				</th>
+			</tr>
+		</thead>
+		<tfoot>
+			<tr>
+				<td colspan="15">
+					<?php echo $this->pagination->getListFooter(); ?>
+				</td>
+			</tr>
+		</tfoot>
+		<tbody>
+		<?php foreach ($this->rows as $i => $item) : 
+		
+		//Prepare date
+		$displaydate = JemOutput::formatLongDateTime($item->dates, null, $item->enddates, null);
+		// Insert a break between date and enddate if possible
+		$displaydate = str_replace(" - ", " -<br />", $displaydate);
+		
+		//Prepare time
+		if (!$item->times) {
+			$displaytime = '-';
+		} else {
+			$displaytime = JemOutput::formattime($item->times);
+		}
+		
+		if ($item->language && JLanguageMultilang::isEnabled())
+			{
+				$tag = strlen($item->language);
+				if ($tag == 5)
+				{
+					$lang = substr($item->language, 0, 2);
+				}
+				elseif ($tag == 6)
+				{
+					$lang = substr($item->language, 0, 3);
+				}
+				else {
+					$lang = "";
+				}
+			}
+			elseif (!JLanguageMultilang::isEnabled())
+			{
+				$lang = "";
+			}
+			?>
+			<tr class="row<?php echo $i % 2; ?>">
+				<td>
+					<a href="javascript:void(0)" onclick="if (window.parent) window.parent.<?php echo $this->escape($function);?>('<?php echo $item->id; ?>', '<?php echo $this->escape(addslashes($item->title)); ?>', '<?php echo $this->escape(JemHelperRoute::getEventRoute($item->slug)); ?>', '<?php echo $this->escape(JemHelperRoute::getEventRoute($item->slug)); ?>', '<?php echo $this->escape($lang); ?>',null);">
+						<?php echo $this->escape($item->title); ?></a>
+				</td>
+				<td class="center">
+					<?php echo $this->escape($item->access_level); ?>
+				</td>
+				<td class="center">
+					<?php echo implode(", ", JemOutput::getCategoryList($item->categories, $this->jemsettings->catlinklist,true)); ?>
+				</td>
+				<td class="center nowrap">
+					<?php echo $displaydate; ?>
+				</td>
+				<td class="center">
+					<?php echo (int) $item->id; ?>
+				</td>
+			</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
+
+	<div>
+		<input type="hidden" name="task" value="" />
+		<input type="hidden" name="boxchecked" value="0" />
+		<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
+		<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
+		<?php echo JHtml::_('form.token'); ?>
+	</div>
+</form>
