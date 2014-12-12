c7cbbbf1610d4ca750a5ae27c05a852ae41db610
Bluefox <jaapkoelman@hotmail.com>
2014-12-12 23:12:20 +0100

    module: wide, changed display of blank image, REF #174

--- modules/mod_jem_wide/helper.php
+++ modules/mod_jem_wide/helper.php
@@ -163,16 +163,16 @@ abstract class modJEMwideHelper
 			$lists[$i]->catname			= implode(", ", JemOutput::getCategoryList($row->categories, $params->get('linkcategory', 1)));
 	
 			if ($dimage == null) {
-				$lists[$i]->eventimage		= JURI::base(true).'/media/system/images/blank.png';
-				$lists[$i]->eventimageorig	= JURI::base(true).'/media/system/images/blank.png';
+				$lists[$i]->eventimage		= "";
+				$lists[$i]->eventimageorig	= "";
 			} else {
 				$lists[$i]->eventimage		= JURI::base(true).'/'.$dimage['thumb'];
 				$lists[$i]->eventimageorig	= JURI::base(true).'/'.$dimage['original'];
 			}
 	
 			if ($limage == null) {
-				$lists[$i]->venueimage		= JURI::base(true).'/media/system/images/blank.png';
-				$lists[$i]->venueimageorig	= JURI::base(true).'/media/system/images/blank.png';
+				$lists[$i]->venueimage		= "";
+				$lists[$i]->venueimageorig	= "";
 			} else {
 				$lists[$i]->venueimage		= JURI::base(true).'/'.$limage['thumb'];
 				$lists[$i]->venueimageorig	= JURI::base(true).'/'.$limage['original'];
