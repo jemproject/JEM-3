fddbd193c288e808c2d1054f6eab5576b9e6c522
Bluefox <jaapkoelman@hotmail.com>
2015-01-18 10:27:50 +0100

    updated site helper

--- site/helpers/helper.php
+++ site/helpers/helper.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @version 3.0.5
+ * @version 3.0.6
  * @package JEM
- * @copyright (C) 2013-2014 joomlaeventmanager.net
+ * @copyright (C) 2013-2015 joomlaeventmanager.net
  * @copyright (C) 2005-2009 Christoph Lukes
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
@@ -162,7 +162,7 @@ class JemHelper {
 				$query = $db->getQuery(true);
 				$query->update('#__jem_events');
 				$query->set('published = -2');
-				$query->where(array("dates > 0","DATE_SUB(NOW(), INTERVAL ".$jemsettings->minus." DAY) > (IF (enddates IS NOT NULL, enddates, dates))","published=1"));
+				$query->where(array("dates > 0","DATE_SUB(NOW(), INTERVAL ".$db->quote($jemsettings->minus)." DAY) > (IF (enddates IS NOT NULL, enddates, dates))","published=1"));
 				$db->SetQuery($query);
 				$db->execute();
 			}
@@ -172,7 +172,7 @@ class JemHelper {
 				$query = $db->getQuery(true);
 				$query->update('#__jem_events');
 				$query->set('published = 2');
-				$query->where(array("dates > 0","DATE_SUB(NOW(), INTERVAL ".$jemsettings->minus." DAY) > (IF (enddates IS NOT NULL, enddates, dates))","published=1"));
+				$query->where(array("dates > 0","DATE_SUB(NOW(), INTERVAL ".$db->quote($jemsettings->minus)." DAY) > (IF (enddates IS NOT NULL, enddates, dates))","published=1"));
 				$db->SetQuery($query);
 				$db->execute();
 			}
