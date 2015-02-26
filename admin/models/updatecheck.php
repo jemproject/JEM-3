50c1cc6dcabf1944efcb3d7b7321e1f5ea65ab5d
Bluefox <jaapkoelman@hotmail.com>
2015-02-26 19:20:02 +0100

    model-updatecheck: changed link of updatefile

--- admin/models/updatecheck.php
+++ admin/models/updatecheck.php
@@ -30,7 +30,7 @@ class JemModelUpdatecheck extends JModelLegacy
 	function getUpdatedata()
 	{
 		$installedversion	= JemHelper::getParam(1,'version',1,'com_jem');
-		$updateFile			= "http://www.joomlaeventmanager.com/updatecheck/jem-3-update.xml";
+		$updateFile			= "http://www.joomlaeventmanager.com/updatecheck/3/updateview.xml";
 		$checkFile			= self::CheckFile($updateFile);
 		$updatedata 		= new stdClass();
 
