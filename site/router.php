ab230aebe2e6f01f71ec0ae5eab2e3c285162b0c
Jaap <jaapkoelman@hotmail.com>
2015-03-15 13:11:46 +0100

    changed router.php
    the class is now extending JComponent

--- site/router.php
+++ site/router.php
@@ -7,157 +7,162 @@
  */
 defined('_JEXEC') or die;
 
-function jemBuildRoute(&$query)
-{
-	$segments = array();
 
-	if (isset($query['view'])) {
-		$segments[] = $query['view'];
-		unset($query['view']);
+class JemRouter extends JComponentRouterBase
+{
+	public function build(&$query) {
+		$segments = array();
+		
+		if (isset($query['view'])) {
+			$segments[] = $query['view'];
+			unset($query['view']);
+		}
+		
+		if (isset($query['id'])) {
+			$segments[] = $query['id'];
+			unset($query['id']);
+		};
+		
+		return $segments;
 	}
-
-	if (isset($query['id'])) {
-		$segments[] = $query['id'];
-		unset($query['id']);
-	};
-
-	return $segments;
-}
-
-function jemParseRoute($segments)
-{
-	$vars = array();
-
-	// Count segments
-	$count = count($segments);
-
-
-	// Handle View and Identifier
-	switch ($segments[0])
+	
+	
+	public function parse(&$segments)
 	{
-		case 'category':
-			{
-				if ($count == 2) {
+		$vars = array();
+		
+		// Count segments
+		$count = count($segments);
+		
+		
+		// Handle View and Identifier
+		switch ($segments[0])
+		{
+			case 'category':
+				{
+					if ($count == 2) {
+						$id = explode(':', $segments[1]);
+						$vars['id'] = $id[0];
+						$vars['view'] = 'category';
+					} else {
+						$vars['view'] = 'category';
+					}
+				}
+				break;
+		
+			case 'event':
+				{
+					if ($count == 2) {
+						$id = explode(':', $segments[1]);
+						$vars['id'] = $id[0];
+						$vars['view'] = 'event';
+					} else {
+						$vars['view'] = 'event';
+					}
+				}
+				break;
+		
+			case 'venue':
+				{
+					if ($count == 2) {
+						$id = explode(':', $segments[1]);
+						$vars['id'] = $id[0];
+						$vars['view'] = 'venue';
+					} else {
+						$vars['view'] = 'venue';
+					}
+				}
+				break;
+		
+			case 'editvenue':
+				{
+					$vars['view'] = 'editvenue';
+					if ($count == 2) {
+						$vars['id'] = $segments[1];
+					}
+				}
+				break;
+		
+			case 'eventslist':
+				{
+					$vars['view'] = 'eventslist';
+				}
+				break;
+		
+			case 'search':
+				{
+					$vars['view'] = 'search';
+				}
+				break;
+		
+			case 'categoriesdetailed':
+				{
+					$vars['view'] = 'categoriesdetailed';
+				}
+				break;
+		
+			case 'categories':
+				{
+					$vars['view'] = 'categories';
+				}
+				break;
+		
+			case 'calendar':
+				{
+					// $id = explode(':', $segments[1]);
+					// $vars['id'] = $id[0];
+					$vars['view'] = 'calendar';
+				}
+				break;
+		
+			case 'venues':
+				{
+					$vars['view'] = 'venues';
+				}
+				break;
+		
+			case 'day':
+				{
+					$vars['view'] = 'day';
+					if ($count == 2) {
+						$vars['id'] = $segments[1];
+					}
+				}
+				break;
+		
+			case 'myattendances':
+				{
+					$vars['view'] = 'myattendances';
+				}
+				break;
+		
+			case 'myevents':
+				{
+					$vars['view'] = 'myevents';
+				}
+				break;
+		
+			case 'myvenues':
+				{
+					$vars['view'] = 'myvenues';
+				}
+				break;
+		
+			case 'attendees':
+				{
 					$id = explode(':', $segments[1]);
 					$vars['id'] = $id[0];
-					$vars['view'] = 'category';
-				} else {
-					$vars['view'] = 'category';
+					$vars['view'] = 'attendees';
 				}
-			}
-			break;
-
-		case 'event':
-			{
-				if ($count == 2) {
-					$id = explode(':', $segments[1]);
-					$vars['id'] = $id[0];
-					$vars['view'] = 'event';
-				} else {
-					$vars['view'] = 'event';
+				break;
+		
+			default:
+				{
+					$vars['view'] = $segments[0];
 				}
-			}
-			break;
-
-		case 'venue':
-			{
-				if ($count == 2) {
-					$id = explode(':', $segments[1]);
-					$vars['id'] = $id[0];
-					$vars['view'] = 'venue';
-				} else {
-					$vars['view'] = 'venue';
-				}
-			}
-			break;
-
-		case 'editvenue':
-			{
-				$vars['view'] = 'editvenue';
-				if ($count == 2) {
-					$vars['id'] = $segments[1];
-				}
-			}
-			break;
-
-		case 'eventslist':
-			{
-				$vars['view'] = 'eventslist';
-			}
-			break;
-
-		case 'search':
-			{
-				$vars['view'] = 'search';
-			}
-			break;
-
-		case 'categoriesdetailed':
-			{
-				$vars['view'] = 'categoriesdetailed';
-			}
-			break;
-
-		case 'categories':
-			{
-				$vars['view'] = 'categories';
-			}
-			break;
-
-		case 'calendar':
-			{
-				// $id = explode(':', $segments[1]);
-				// $vars['id'] = $id[0];
-				$vars['view'] = 'calendar';
-			}
-			break;
-
-		case 'venues':
-			{
-				$vars['view'] = 'venues';
-			}
-			break;
-
-		case 'day':
-			{
-				$vars['view'] = 'day';
-				if ($count == 2) {
-					$vars['id'] = $segments[1];
-				}
-			}
-			break;
-
-		case 'myattendances':
-			{
-				$vars['view'] = 'myattendances';
-			}
-			break;
-
-		case 'myevents':
-			{
-				$vars['view'] = 'myevents';
-			}
-			break;
-
-		case 'myvenues':
-			{
-				$vars['view'] = 'myvenues';
-			}
-			break;
-
-		case 'attendees':
-			{
-				$id = explode(':', $segments[1]);
-				$vars['id'] = $id[0];
-				$vars['view'] = 'attendees';
-			}
-			break;
-
-		default:
-			{
-				$vars['view'] = $segments[0];
-			}
-			break;
+				break;
+		}
+		return $vars;
 	}
-	return $vars;
+	
 }
